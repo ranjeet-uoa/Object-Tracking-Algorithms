bf738ae909d34e56de87f9874715b3c5d5386cd5
ranjeet-uoa <a1233065@adelaide.edu.au>
2024-12-13 14:23:35 +1030

    Delete CKF_and_PF_for_maneuvring_target_1 directory

--- CKF_and_PF_for_maneuvring_target_1/PF/demo.m
+++
@@ -1,9 +0,0 @@
-% This is a demo script for the single target filter using Particle Filter
-clearvars;clc;close all;
-%rng(0);
-model= gen_model;
-truth= gen_truth(model);
-meas=  gen_meas(model,truth);
-est=   run_filter_pf(model,meas, 'TPD');
-[handles,rmse]= plot_results(model,truth,meas,est);
-fprintf('Averaged Root Mean Square Error using Particle Filter is %.2f [m]\n',mean(rmse));
